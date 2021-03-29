dbb3f223dd1beeee729dbe3c8e701f959bedee36
Ranjeet Harishchandre <ranjeetharishchandre@gmail.com>
2021-03-29 22:41:02 +0530

    start of project at 53:24 on 290321 1040pm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import jumboData from "./fixtures/jumbo.json";
+import Jumbotron from "./components/jumbotron/index";
 
- export default function App() {
+export default function App() {
   return (
-    
-      <h1>netflix</h1>
-   
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
-
-
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
